e6cd72361d346853bd4983f988ec8abcd0e61171
Nam Tran <namtran211101@gmail.com>
2021-12-11 10:03:36 +1100

    Initial program skeleton

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "communication.h"
+#include "memory.h"
+#include "supercap.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
