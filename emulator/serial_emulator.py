ee9729670aca8b71d1e43b7c6198eed76f6f7dff
Nam Tran <namtran211101@gmail.com>
2021-12-15 23:34:03 +1100

    Add payload generation for the emulator

--- emulator/serial_emulator.py
+++ emulator/serial_emulator.py
@@ -1,6 +1,7 @@
 import time
 import serial
 import threading
+import random
 
 ser = serial.Serial('COM3',4800,timeout=0.1)
 count = 0
@@ -10,17 +11,27 @@ print('Connected to: ',ser.name, 'Press any key to exit')
 def serialTX():
     global running
     while running:
+        tx_len = random.randint(10,124)
         packet = bytearray()
-        packet.append(0x19)
-        packet.append(0x94)
-        packet.append(0)
-        packet.append(0x7C) #packet length
-        packet.append(0)
-        packet.append(0x08)
-        packet.append(0x86)
+        packet.append(0x19)     # Start flag MSB
+        packet.append(0x94)     # Start flag LSB
+        packet.append(0x00)     # Packet length MSB
+        packet.append(tx_len)   # Packet length LSB
+        packet.append(0x00)     # Protocol - ignored
+        packet.append(0x08)     # Device ID MSB
+        packet.append(0x86)     # Device ID LSB
+        packet.append(0x00)     # Message ID - ignored
+        packet.append(0x00)
+        packet.append(0x00)
+        packet.append(0x01)     # Transfer/Response Code - 0x01 for issure data
+
+        # 11 bytes before actual data
+        for i in range(tx_len-2):
+            packet.append(random.randint(1,255))
         ser.write(packet)
         print("Packet sent: ",packet.hex())
-        time.sleep(0.1)
+        print("Random length: ",tx_len+11)
+        time.sleep(1.5)
     ser.close()
 
 def serialRX():
