fe350a2433f8e6f5c62ed43d1b7e68d701c2180c
Nam Tran <namtran211101@gmail.com>
2022-02-06 23:27:09 +1100

    Completed state machine with arbitrary data

--- include/memory.h
+++ include/memory.h
@@ -1,21 +1,23 @@
-struct ExpFields { // 6 bytes
+#define EXP_FIELDS_SIZE 6
+
+struct DataPointFields { // 6 bytes
     uint8_t time[2];
     uint8_t voltage[2];
     uint8_t current[2];
 };
 
 // 115 max
-struct PayloadFields {  // 104 bytes
-    uint16_t exprimentNo; // 2 bytes
-    uint16_t startTemp; // 2 bytes
-    ExpFields chargeData[8]; //6*8 = 48 bytes  
+struct ExprimentFields {  // 104 bytes
+    uint8_t exprimentNo[2]; // 2 bytes
+    uint8_t startTemp[2]; // 2 bytes
+    DataPointFields chargeData[8]; //6*8 = 48 bytes  
     uint16_t midTemp; // 2 bytes
-    ExpFields dischargeData[8]; //6*8 = 48 bytes
+    DataPointFields dischargeData[8]; //6*8 = 48 bytes
     uint16_t endTemp;   // 2 bytes
 };
 
-union PayloadBuffer
+union ExprimentBuffer
 {
-    PayloadFields fields;
+    ExprimentFields fields;
     uint8_t data[104];
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <CircularBuffer.h>
 #include "TeensyTimerTool.h"
 #include "communication.h"
 #include "memory.h"
 #include "supercap.h"
 
 using namespace TeensyTimerTool;
+
+#define DATA_COLLECT_INTERTVAL 10 //ms
 
 typedef enum CAP_STATE {
   CAP_STATE_IDLE,
@@ -16,82 +19,126 @@ typedef enum CAP_STATE {
 
 
 //global variables
-static volatile int cap_state;
-static int ticker_attached = 0;
-static int current_state = CAP_STATE_IDLE;
-static int prev_state = CAP_STATE_DISCHARGE;
+byte dataPointCounter = 0;
+CircularBuffer<ExprimentBuffer,4> transmitBuffer; // contain 4 payload, each for a supercap waiting to be transmitted
+CircularBuffer<ExprimentBuffer,8> transmitQueue; // contain 2 experiment data packet, each has 4 payload waiting to copied to the transmit buffer
 
 
-SuperCap SuperCap1(4,15);
-SuperCap SuperCap2();
-SuperCap SuperCap3();
-SuperCap SuperCap4();
-OneShotTimer StateTicker(TCK);
+//SuperCap superCaps[] = {SuperCap(1,2),SuperCap(1,2),SuperCap(1,2),SuperCap(1,2)};
 
 const int ledPin = 13;
 UART obcUART;
 
-// void heartbeat() {
-//     static byte heartState = 1;
-// 	static unsigned long previousBeat = millis();
-// 	unsigned long currentMillis = millis();
-// 	if (currentMillis - previousBeat >= 500) {
-// 		previousBeat = currentMillis;
-// 		heartState ^= 1;
-// 		digitalWrite(ledPin, heartState);
-// 	}
-// }
-
-
-void changeState(int next_state) {
-  ticker_attached = 0;
-  current_state = next_state;
-}
-
 // state daemon of the capacitor
-void stateDaemon(){
-    static unsigned long previous_state_time = millis();
-    unsigned long current_time;
-    switch (cap_state) {
-        case CAP_STATE_IDLE:
-            //SuperCap1.setIdle();
-            //SuperCap1.setCurrent(0);
-            current_time = millis();
-            if (abs(current_time - previous_state_time) >= 5) {
-                prev_state = CAP_STATE_IDLE;
-                if (prev_state == CAP_STATE_DISCHARGE) {
-                    Serial.println("Changing to Charge");
-                    changeState(CAP_STATE_CHARGE);
-                }
-                else {
-                    Serial.println("Changing to Discharge");
-                    changeState(CAP_STATE_DISCHARGE);
-                }
-            }
-            break;
+void stateDaemon()
+{
+	static int experimentCounter = 0;
+	static unsigned long startTime = 0;
+	static byte currentState = CAP_STATE_CHARGE;
+	static byte previousState = CAP_STATE_IDLE;
+	static ExprimentBuffer expBuffers[4];
+
+    switch (currentState) {
+        case CAP_STATE_IDLE: //rest for 62 ms
+		{
+			dataPointCounter++;
+        	if (dataPointCounter > 1) // reset the counter and change state
+			{
+          		dataPointCounter = 0;
+				if (previousState == CAP_STATE_DISCHARGE) //switch between discharge and charge
+				{
+					currentState = CAP_STATE_CHARGE;
+					//Serial.println("Changing State... Next State: CHARGE");
+				} 
+				else 
+				{
+					currentState = CAP_STATE_DISCHARGE;
+					//Serial.println("Changing State... Next State: DISCHARGE");
+				}
+				previousState = CAP_STATE_IDLE;
+        	}	
+			break;
+		}
+                    
 
         case CAP_STATE_CHARGE:
-            //SuperCap1.setIdle();
-            //SuperCap1.setCurrent(3);
-            current_time = millis();
-            if (abs(current_time - previous_state_time) >= 2) {
-                Serial.println("Changing to IDLE from CHARGE");
-                prev_state = CAP_STATE_CHARGE;
-                changeState(CAP_STATE_IDLE);
-            }
-            break;
+		{
+			dataPointCounter++;
+			if (dataPointCounter == 1) 
+			{
+				startTime = millis(); // record start time
+				experimentCounter++;
+				for (byte i=0; i<4; i++) {
+					expBuffers[i].fields.exprimentNo[0] = (experimentCounter >> 8) & 0xFF;
+					expBuffers[i].fields.exprimentNo[1] = experimentCounter & 0xFF;
+					expBuffers[i].fields.startTemp[0] = (20 >> 8) & 0xFF; // record start temperature
+					expBuffers[i].fields.startTemp[1] = 20 & 0xFF;
+				}
+			}
+			
+			// record time, voltage and current
+			for (byte i=0; i<4; i++) {
+				unsigned int deltaTime = abs(millis() - startTime);
+				expBuffers[i].fields.chargeData[dataPointCounter-1].time[0] = (deltaTime >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.chargeData[dataPointCounter-1].time[1] = deltaTime & 0xFF;
+				expBuffers[i].fields.chargeData[dataPointCounter-1].voltage[0] = (12 >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.chargeData[dataPointCounter-1].voltage[1] = 12 & 0xFF;
+				expBuffers[i].fields.chargeData[dataPointCounter-1].current[0] = (43 >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.chargeData[dataPointCounter-1].current[1] = 43 & 0xFF;
+			}
+
+			// reset counter and change state once have more than 8 points
+        	if (dataPointCounter > 8) 
+			{
+          		dataPointCounter = 0;
+				previousState = CAP_STATE_CHARGE;
+				currentState = CAP_STATE_IDLE;
+        	}	
+			break;
+		}
 
         case CAP_STATE_DISCHARGE:
-            //SuperCap1.setIdle();
-            //SuperCap1.setCurrent(-3);
-            current_time = millis();
-            if (abs(current_time - previous_state_time) >= 2) {
-                Serial.println("Changing to IDLE form DIS");
-                prev_state = CAP_STATE_DISCHARGE;
-                changeState(CAP_STATE_IDLE);
-            }
-            break;
-        
+ 		{
+			dataPointCounter++;
+			if (dataPointCounter == 1) 
+			{
+				for (byte i=0; i<4; i++) {
+					expBuffers[i].fields.midTemp = 22; // record start temperature
+				}
+			}
+			
+			// record time, voltage and current
+			for (byte i=0; i<4; i++) {
+				unsigned int deltaTime = abs(millis() - startTime);
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].time[0] = (deltaTime >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].time[1] = deltaTime & 0xFF;
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].voltage[0] = (11 >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].voltage[1] = 11 & 0xFF;
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].current[0] = (23 >> 8) & 0xFF; // remember to handle this
+				expBuffers[i].fields.dischargeData[dataPointCounter-1].current[1] = 23 & 0xFF;
+			}
+
+			// reset counter and change state once have more than 8 points
+        	if (dataPointCounter > 8) 
+			{
+				for (byte i=0; i<4; i++) {
+					expBuffers[i].fields.endTemp = 22; // record start temperature
+				}
+          		dataPointCounter = 0;
+				previousState = CAP_STATE_DISCHARGE;
+				currentState = CAP_STATE_IDLE;
+				// push into the transmit queue
+				for (byte i=0; i<4; i++) {
+					transmitQueue.push(expBuffers[i]);
+				}
+				// uint8_t test[104];
+				// for (byte i=0; i<104; i++) {
+				// 	test[i] = transmitQueue[0].data[i];
+				// }
+				// obcUART.transmit(test,104);
+        	}	
+			break;
+		}
   }
 }
 
@@ -99,12 +146,17 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   obcUART.begin();
 
-  SuperCap1.readCurrent();
-  SuperCap1.begin(0x88,&Wire1);
+  //SuperCap1.readCurrent();
+  //SuperCap1.begin(0x88,&Wire1);
 }
 
 void loop() {
-  //heartbeat();
-  //serialHandler.handler();
+	static unsigned long previousBeat = millis();
+	unsigned long currentMillis = millis();
+	unsigned int deltaTime = abs(currentMillis - previousBeat);
+	if (deltaTime >= DATA_COLLECT_INTERTVAL) {
+		previousBeat = currentMillis;
+    	stateDaemon();
+	}
 }
 
